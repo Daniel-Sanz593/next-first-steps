a30f04dc81fdbf67633c1383ebf54220a195a3d2
Daniel Sanz Garcia <daniel.sanz@vasscompany.com>
2024-06-20 11:33:39 +0200

    First commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,21 @@
+
+import type{ Metadata } from "next";
+
+
+export const metadata: Metadata = {
+ title: 'SEO Contacto Title',
+ description: 'SEO Contacto  Title',
+ keywords: ['About Page','Fernando','información','...'],
+};
+
+export default function ContactPage(){
+
+    return(
+        <>
+        <span className="text text-7xl">Contact Page</span>      
+        </>
+
+    )
+
+
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,20 @@
+import type{ Metadata } from "next";
+
+
+export const metadata: Metadata = {
+ title: 'SEO Pricin Title',
+ description: 'SEO Pricin  Title',
+ keywords: ['About Page','Fernando','información','...'],
+};
+
+export default function PricinPage(){
+
+    return(
+        <>
+        <span className="text text-7xl">Pricin Page</span>      
+        </>
+
+    )
+
+
+}
